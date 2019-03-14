69465a0cfea37100c4bca634e4b67175f22a1f76
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2019-03-14 13:42:36 +0700

    working on dashboard component

--- employeemanager/src/components/Dashboard.vue
+++ employeemanager/src/components/Dashboard.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="dashboard">
-        <h3>Dashboard</h3>
+        <ul class="collection with-header">
+
+            <li class="collection-header">
+                <h4>Employees</h4>
+            </li>
+
+            <li class="collection-item" v-for="employee in employees" :key="employee.id">
+                <div class="chip">{{employee.dept}}</div>
+                {{employee.employee_id}} : {{employee.name}}
+                
+                <router-link v-bind:to="{name:'view-employee', params: {employee_id: employee.employee_id}}" class="secondary-content">
+                    <li class="fa fa-eye"></li>
+                </router-link>
+            </li>
+        </ul>
 
         <div class="fixed-action-btn">
             <router-link to="/new" class="btn-floating btn-large red">
@@ -10,13 +24,30 @@
     </div>
 </template>
 <script>
+import db from './firebaseInit.js'
 export default {
     name: 'dashboard',
     data(){
         return {
-
+            employees: []
         }
-    }
+    },
+    created() {
+        db.collection('employees').orderBy('dept').get().then
+        (querySnapshot => {
+            querySnapshot.forEach(doc => {
+                const data = {
+                    'id' : doc.id,
+                    'employee_id' : doc.data().employee_id,
+                    'name' : doc.data().name,
+                    'dept' : doc.data().dept
+                }
+                this.employees.push(data);
+                console.log(this.employees);
+                
+            });
+        })
+    },
 }
 </script>
 <style>
--- employeemanager/src/components/firebaseInit.js
+++ employeemanager/src/components/firebaseInit.js
@@ -1,4 +1,4 @@
-import firebase from 'firebse'
+import firebase from 'firebase'
 import 'firebase/firestore' 
 import firebaseConfig from './firebaseConfig'
 
